fc510fbb2c83599ffd346343084aead09853f335
Evan-Callaghan <callaghan.evan@gmail.com>
2023-07-24 12:03:23 -0300

    Additions

--- simTt_mod.R
+++ simTt_mod.R
@@ -75,3 +75,42 @@ plot(0:99, Tt$value, type = 'l'); grid()
 
 
 
+
+
+source('initialize.R')
+source('estimate.R')
+
+
+set.seed(12)
+X = interpTools::simXt(N = 500, numTrend = 2, numFreq = 2, b = c(10, 0.5), w = c(0.5, 10))$Xt
+X0 = interpTools::simulateGaps(list(X), p = 0.2, g = 50, K = 1)[[1]]$p0.2$g50[[1]]
+
+## Plot 1: Original and gapped time series 
+plot(X, type = 'l', lty = 3); grid()
+lines(X0, type = 'l', lwd = 2)
+
+## Plot 2: Initialize Step
+XI = initialize(X0)
+plot(XI, type = 'l', col = 'red', lty = 3)
+lines(X0, type = 'l', lwd = 2); grid()
+
+## Plot 3a: Estimate Trend Step
+XE_Mt = estimator(XI, method = 'Mt')
+plot(XI, type = 'l', col = 'red', lty = 3)
+lines(X0, type = 'l', lwd = 2); grid()
+lines(XE_Mt[,1], type = 'l', lty = 2, col = 'dodgerblue')
+
+## Plot 3b: Estimate Periodic Step
+XE_Tt = estimator(XI - XE_Mt, method = 'Tt')
+plot(XI - XE_Mt, type = 'l', lwd = 2)
+lines(XE_Tt, type = 'l', col = 'dodgerblue')
+
+## Plot 3c: Estimate Both Together
+XE = estimator(XI, method = 'Xt')
+plot(XI, type = 'l', col = 'red', lty = 3)
+lines(X0, type = 'l', lwd = 2); grid()
+lines(XE, type = 'l', lty = 2, col = 'dodgerblue')
+
+
+
+
