c4056480ca1d6322c96d1f64bb92cb2f8a17f65a
Evan-Callaghan <callaghan.evan@gmail.com>
2023-07-27 15:47:44 -0300

    Autoencoders

--- Exploration/autoencoders.R
+++ Exploration/autoencoders.R
@@ -78,21 +78,3 @@ lines(preds, type = 'l', lwd = 2)
 
 
 
-# define input sequence
-sequence = array([0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9])
-# reshape input into [samples, timesteps, features]
-n_in = len(sequence)
-sequence = sequence.reshape((1, n_in, 1))
-# define model
-model = Sequential()
-model.add(LSTM(100, activation='relu', input_shape=(n_in,1)))
-model.add(RepeatVector(n_in))
-model.add(LSTM(100, activation='relu', return_sequences=True))
-model.add(TimeDistributed(Dense(1)))
-model.compile(optimizer='adam', loss='mse')
-# fit model
-model.fit(sequence, sequence, epochs=300, verbose=0)
-plot_model(model, show_shapes=True, to_file='reconstruct_lstm_autoencoder.png')
-# demonstrate recreation
-yhat = model.predict(sequence, verbose=0)
-print(yhat[0,:,0])
