aa13c631c034ed5eaf35e40e0332fb0ed8e4e089
Evan-Callaghan <callaghan.evan@gmail.com>
2023-08-17 09:59:37 -0400

    Blocking function updates

--- block_function.R
+++ block_function.R
@@ -3,6 +3,7 @@
 
 
 library(dplyr)
+library(interpTools)
 
 block_function <- function(x, window, forecast){
   
@@ -10,104 +11,52 @@ block_function <- function(x, window, forecast){
 
 
 set.seed(52)
-X = interpTools::simXt(N = 1000, numTrend = 1, numFreq = 2)$Xt
-X0 = interpTools::simulateGaps(list(X), p = 0.1, g = 10, K = 1)[[1]]$p0.1$g10[[1]]
-
-
-
-N = length(X0)
-mask = ifelse(is.na(X0), TRUE, FALSE)
-
-valid_index = which(mask == FALSE)
-invalid_index = which(mask == TRUE)
-
-valid_diff = which(valid_index[-1] - valid_index[-length(valid_index)] > 1)
-invalid_diff = which(invalid_index[-1] - invalid_index[-length(invalid_index)] > 1)
-
-
-
-
-
-check = mask[1]
+X = interpTools::simXt(N = 100, numTrend = 1, numFreq = 2)$Xt
+X0 = interpTools::simulateGaps(list(X), p = 0.1, g = 2, K = 1)[[1]]$p0.1$g2[[1]]
 
 
 
 
 
 
-storage = c()
-for (i in 1:N){
-  
-  if (!is.na(i)){
-    storage = c(storage, i)
-  }
-  
-}
-
-
-!(1 %in% c(1, 2, 3))
-
-
-mask = which(is.na(X0))
-
-
+mask = ifelse(is.na(X0), NA, TRUE)
+missing_block = findBlocks(mask)
+missing_block
 
 N = length(X0)
-for (i in 1:N){
-  
-  if (is.na(X0[i])){
-    start = i
-      
-    while (is.na(X0[i])){
-      end = i
-      i = i + 1
-    }
-    valid = FALSE
+M = dim(missing_block)[1] + 1
+WINDOW = 5
+FORECAST = 1
+training_block = data.frame(Start = rep(NA, M), End = rep(NA, M), 
+                            Gap = rep(NA, M), Train = rep(NA, M))
+
+for (i in 1:M){
+  if (i == 1){
+    training_block[i,1:2] = c(1, missing_block$Start[1]-1) 
+  }
+  else if (i == M){
+    training_block[i,1:2] = c(missing_block$End[i-1]+1, N)
+  }
+  else{
+    training_block[i,1:2] = c(missing_block$End[i-1]+1, missing_block$Start[i]-1)
   }
 }
+training_block$Gap = training_block$End - training_block$Start + 1
+training_block$Train = TRUE
+
+
+block = rbind(missing_block, training_block) %>% dplyr::arrange(Start)
+block$Valid = ifelse(block$Train == FALSE, NA, 
+       ifelse(block$Gap >= (WINDOW + FORECAST), TRUE, FALSE))
+
+block
 
 
 
 
 
 
-N = length(X0)
-diffs = which(mask[-1] - mask[-length(mask)] > 1)
 
-
-# case: 1 gap only, possibly no gaps
-if (length(diffs)==0){
-  blocks <- matrix(data = 0, nrow = 1, ncol = 3)
-  blocks[1, 1:2] <- c(mask[1], mask[length(mask)])
-}
-else{
-  blocks <- matrix(data = 0, nrow = length(mask), ncol = 5)
-  blocks[1,1] = 1
-  blocks[1,2] = mask[1] - 1
-  
-  blocks[2, 1] <- mask[1]
-  blocks[2, 2] <- mask[diffs[1]]
-  k = 2
-  for (j in 1:length(diffs)){
-    k = k + 1
-    blocks[k, 1:2] <- c(mask[diffs[j]+1], mask[diffs[j+1]])
-  }
-  blocks[k,2] <- max(mask)
-  blocks <- blocks[1:k, ]
-}
-blocks[,3] <- blocks[,2] - blocks[,1] + 1
-
-# checks to remove start/end of sequence
-if(blocks[1,1]==1) {
-  blocks <- blocks[-1, ]
-}
-if(blocks[length(blocks[,1]),2]==Nlen) {
-  blocks <- blocks[-length(blocks[,1]), ] 
-}
-} else {
-  blocks <- NULL
-  
-  
   
   
 
@@ -128,10 +77,10 @@ findBlocks <- function(mask) {
     
     # case: 1 gap only, possibly no gaps
     if(length(diffs)==0) {
-      blocks <- matrix(data=0, nrow=1, ncol=3)
+      blocks <- matrix(data=0, nrow=1, ncol=2)
       blocks[1, 1:2] <- c(mask[1], mask[length(mask)])
     } else {
-      blocks <- matrix(data=0,nrow=length(mask),ncol=3)
+      blocks <- matrix(data=0,nrow=length(mask),ncol=2)
       blocks[1, 1] <- mask[1]
       blocks[1, 2] <- mask[diffs[1]]
       k <- 1
@@ -142,7 +91,6 @@ findBlocks <- function(mask) {
       blocks[k,2] <- max(mask)
       blocks <- blocks[1:k, ]
     }
-    blocks[,3] <- blocks[,2] - blocks[,1] + 1
     
     # checks to remove start/end of sequence
     if(blocks[1,1]==1) {
@@ -154,6 +102,9 @@ findBlocks <- function(mask) {
   } else {
     blocks <- NULL
   }
-  blocks
+  blocks = as.data.frame(blocks) %>% dplyr::rename(Start = V1, End = V2)
+  blocks$Gap = blocks$End - blocks$Start + 1
+  blocks$Train = FALSE
+  return(blocks)
 }
   
