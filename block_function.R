ce67982a197dd0579356f1183438f1425e6c9f8a
Evan-Callaghan <callaghan.evan@gmail.com>
2023-08-18 19:10:50 -0400

    Block function ready

--- block_function.R
+++ block_function.R
@@ -51,19 +51,9 @@ my_block = block_function(X0, window = 5, forecast = 1)
 
 which(my_block$Valid == TRUE)
 
-
-
-
-
 training_index = which(my_block$Valid == TRUE)
 prediction_index = which(is.na(my_block$Valid))
-  
 
-  
-  
-  
-  
-  
 ## findMissingBlock function adapted from tsinterp's findBlocks function
 ##
 ## Takes data mask as input and returns block info.
@@ -108,4 +98,110 @@ findMissingBlock <- function(mask) {
   blocks$Train = FALSE
   return(blocks)
 }
+
+
+
+
+
+
+
+
+
+## ---------------------------------------------
+
+
+blocking <- function(x, window, forecast){
+  
+  ## If necessary, returning error that entire series is NA
+  if (sum(is.na(x)) == length(x)){print('Entire series is NA.'); return(NULL)}
+  
+  ## If necessary, removing missing values from beginning and end of series
+  while(is.na(x[1])){x = x[-1]}
+  while(is.na(x[length(x)])){x = x[-length(x)]}
+  
+  ## Defining parameters and initializing vector to store results
+  N = length(x)
+  block = c()
+  condition = TRUE; i = 1; M = 0
+  
+  while(condition){
+    
+    ## When the current value is not null:
+    if (!is.na(x[i])){
+      continue = TRUE
+      start_idx = i
+      while(continue){
+        i = i + 1
+        if (!is.na(x[i])){next}
+        else{end_idx = i - 1; continue = FALSE}
+        }
+      M = M + 1; seq_info = c(start_idx, end_idx, end_idx - start_idx + 1, 1)
+      block = c(block, seq_info)
+      }
+    
+    ## When the current value is null:
+    else{
+      continue = TRUE
+      start_idx = i
+      while(continue){
+        i = i + 1
+        if (is.na(x[i]) & i <= N){next}
+        else{end_idx = i - 1; continue = FALSE}
+      }
+      M = M + 1; seq_info = c(start_idx, end_idx, end_idx - start_idx + 1, 0)
+      block = c(block, seq_info)
+    }
+    if (i > N){condition = FALSE}
+  }
+  ## Formatting the final block
+  block = as.data.frame(matrix(block, nrow = M, byrow = TRUE), dim = c(M, 4)) %>%
+    dplyr::rename(Start = V1, End = V2, Gap = V3, Complete = V4)
+  block$Complete = ifelse(block$Complete == 0, FALSE, TRUE)
+  block$Valid = ifelse(block$Complete == 0, NA, 
+                       ifelse(block$Gap >= (window + forecast), TRUE, FALSE))
+  return(block)
+}
+## *****************************
+## also want the function to return an updated series if x starts or end with NA values.
+## *****************************
+
+
+x = c(NA, NA, 1, 2, 3, 4, 5, 6, 7, 8, 
+      NA, NA, NA, 10, 11, NA, NA, 2, 3, 4)
+blocking(x, window = 2, forecast = 1)
+
+
+x = c(1, 2, 3, 4, 5, 6, 7, 8, NA, NA, 
+      NA, 10, 11, NA, NA, 2, 3, 4, NA, NA)
+blocking(x, window = 2, forecast = 1)
+
+
+x = c(1, 2, 3, 4, 5, 6, 7, 8, 10, 11, 2, 3, 4)
+blocking(x, window = 2, forecast = 1)
+
+x = c(NA, NA, NA, NA, NA, NA, NA)
+blocking(x, window = 2, forecast = 1)
+
+
+
+
+x = c(1, 2, 3, 4, 5, NA, NA, NA, 9, 10, 11, NA, NA, 14, 15)
+blocking(x, window = 2, forecast = 1)
+
+
+
+set.seed(42)
+X = interpTools::simXt(N = 100)$Xt
+X0 = interpTools::simulateGaps(list(X), p = 0.3, g =5, K = 1)[[1]]$p0.3$g5[[1]]
+X0
+
+blocking(X0, window = 5, forecast = 1)
+
+training_index = which(my_block$Valid == TRUE)
+prediction_index = which(is.na(my_block$Valid))
+
+
+
+
+
   
