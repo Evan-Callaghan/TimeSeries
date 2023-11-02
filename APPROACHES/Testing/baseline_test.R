6248e599af56e0d3aa06d569fdb3ea17c670fdf6
Evan-Callaghan <callaghan.evan@gmail.com>
2023-11-02 15:00:35 -0400

    Adding new series

--- APPROACHES/Testing/baseline_test.R
+++ APPROACHES/Testing/baseline_test.R
@@ -34,7 +34,7 @@ source('APPROACHES/Testing/baseline_helper.R')
 
 P = c(0.1, 0.2, 0.3, 0.4)
 G = c(5, 10, 25, 50)
-K = 50
+K = 100
 
 METHODS = c('NN', 'LI', 'NCS', 'FMM', 'HCS', 'SI', 'KAF', 'KKSF', 'LOCF', 'NOCB', 'SMA', 
             'LWMA', 'EWMA', 'RMEA', 'RMED', 'RMOD', 'RRND', 'HWI')
@@ -47,6 +47,7 @@ numCores = 16
 
 sunspots = read.csv('Data/Cleaned/sunspots.csv')$sunspots
 apple = read.csv('Data/Cleaned/apple.csv')$apple
+temperature = read.csv('Data/Cleaned/toronto_temperature.csv')$temperature
 high_snr = read.csv('Data/Cleaned/simulated.csv')$high_snr
 low_snr = read.csv('Data/Cleaned/simulated.csv')$low_snr
 modulated = read.csv('Data/Cleaned/simulated.csv')$modulated
@@ -59,7 +60,7 @@ modulated = read.csv('Data/Cleaned/simulated.csv')$modulated
 sunspots_sim = simulation_main(sunspots, P, G, K, METHODS, numCores)
 
 ## Exporting simulation performance as a csv file
-write.csv(sunspots_sim, 'APPROACHES/SIMULATIONS/SimRound1_October2023_Baseline_sunspots.csv', row.names = FALSE)
+write.csv(sunspots_sim, 'APPROACHES/SIMULATIONS/SimRound1_October2023_Baseline_sunspots_K100.csv', row.names = FALSE)
 
 
 
@@ -70,38 +71,49 @@ write.csv(sunspots_sim, 'APPROACHES/SIMULATIONS/SimRound1_October2023_Baseline_s
 apple_sim = simulation_main(apple, P, G, K, METHODS, numCores)
 
 ## Exporting simulation performance as a csv file
-write.csv(apple_sim, 'APPROACHES/SIMULATIONS/SimRound1_October2023_Baseline_apple.csv', row.names = FALSE)
+write.csv(apple_sim, 'APPROACHES/SIMULATIONS/SimRound1_October2023_Baseline_apple_K100.csv', row.names = FALSE)
 
 
 
-## Simulation 3: High SNR Time Series
+## Simulation 3: Temperature Data
+## -----------------------
+
+## Running the imputation simulation
+temperature_sim = simulation_main(temperature, P, G, K, METHODS, numCores)
+
+## Exporting simulation performance as a csv file
+write.csv(temperature_sim, 'APPROACHES/SIMULATIONS/SimRound1_October2023_Baseline_temperature_K100.csv', row.names = FALSE)
+
+
+
+## Simulation 4: High SNR Time Series
 ## -----------------------
 
 ## Running the imputation simulation
 high_snr_sim = simulation_main(high_snr, P, G, K, METHODS, numCores)
 
 ## Exporting simulation performance as a csv file
-write.csv(high_snr_sim, 'APPROACHES/SIMULATIONS/SimRound1_October2023_Baseline_high_snr.csv', row.names = FALSE)
+write.csv(high_snr_sim, 'APPROACHES/SIMULATIONS/SimRound1_October2023_Baseline_high_snr_K100.csv', row.names = FALSE)
 
 
 
-## Simulation 4: Low SNR Time Series
+## Simulation 5: Low SNR Time Series
 ## -----------------------
 
 ## Running the imputation simulation
 low_snr_sim = simulation_main(low_snr, P, G, K, METHODS, numCores)
 
 ## Exporting simulation performance as a csv file
-write.csv(low_snr_sim, 'APPROACHES/SIMULATIONS/SimRound1_October2023_Baseline_low_snr.csv', row.names = FALSE)
+write.csv(low_snr_sim, 'APPROACHES/SIMULATIONS/SimRound1_October2023_Baseline_low_snr_K100.csv', row.names = FALSE)
 
 
 
-## Simulation 5: Frequency Modulated Time Series
+## Simulation 6: Frequency Modulated Time Series
 ## -----------------------
 
 ## Running the imputation simulation
 modulated_sim = simulation_main(modulated, P, G, K, METHODS, numCores)
 
 ## Exporting simulation performance as a csv file
-write.csv(modulated_sim, 'APPROACHES/SIMULATIONS/SimRound1_October2023_Baseline_modulated.csv', row.names = FALSE)
+write.csv(modulated_sim, 'APPROACHES/SIMULATIONS/SimRound1_October2023_Baseline_modulated_K100.csv', row.names = FALSE)
 
