a809a8f7e9a813b4f719b329d880a5bc77e8746b
Evan-Callaghan <callaghan.evan@gmail.com>
2023-11-03 18:38:15 -0400

    Small updates to plotting functions

--- APPROACHES/Testing/plotting.R
+++ APPROACHES/Testing/plotting.R
@@ -37,11 +37,11 @@ plot_ts <- function(x, title = ''){
 #' 
 #' Function to formalize the imputation simulation plotting process. Follows a standard form for which 
 #' all time series imputation plots/visualization will be displayed in the thesis.
-#' @param aggregation {aggregation_pf}; Aggregation of imputation performance across methods
-#' @param criteria {string}; Desired criteria for imputation performance evaluation (default 'RMSE)
-#' @param agg {string}; Function type for aggregating data across K iterations (default 'mean')
+#' @param data {dataframe}; Data frame containing complete simulation results.
+#' @param metric {string}; Desired criteria for imputation performance evaluation
+#' @param aggregation {string}; Function type for aggregating data across K iterations
+#' @param methods {list}; List of all desired methods to be displayed
 #' @param title {string}; Desired title for the returned plot
-#' @param levels {numeric}; Vector containing the method names in the desired order for the plot
 #'
 simulation_plot <- function(data, metric, aggregation, methods, title = ''){
   
@@ -61,64 +61,77 @@ simulation_plot <- function(data, metric, aggregation, methods, title = ''){
     geom_tile(color = '#204176', linewidth = 0.1) +
     facet_grid(~ factor(Method, levels = methods)) +
     labs(title = title, 
+         subtitle = paste0('Metric: ', metric, '   Aggregation: ', aggregation),
          x = "Missing Proportion (P)", 
-         y = "Gap Width (G)", 
-         fill = paste0(metric, ' (', aggregation, ')')) +
+         y = "Gap Width (G)") +
     scale_fill_gradientn(colours = col, values = c(0,1)) + 
     guides(fill = guide_colourbar(label = TRUE, ticks = TRUE)) +
-    geom_text(aes(label = round(agg_value, 3)), color = "white", size = 4) +
+    geom_text(aes(label = round(agg_value, 3)), color = "white", size = 3.5) +
     theme_bw() +
     theme(plot.title = element_text(hjust = 0.5, face = 'bold', size = 14), 
+          plot.subtitle = element_text(hjust = 0.5, size = 12, face = 'italic'),
           strip.background = element_rect(fill = 'white'), 
-          strip.text = element_text(color = 'black', face = 'bold', size = 12), 
-          panel.spacing = unit(0.8, 'lines'), 
-          axis.text = element_text(color = 'black', size = 12), 
-          axis.title.x = element_text(color = 'black', size = 14, margin = margin(t = 8)), 
-          axis.title.y = element_text(color = 'black', size = 14, margin = margin(r = 8)), 
+          strip.text = element_text(color = 'black', face = 'bold', size = 10), 
+          panel.spacing = unit(0.3, 'lines'), 
+          axis.text = element_text(color = 'black', size = 10), 
+          axis.title.x = element_text(color = 'black', size = 12, margin = margin(t = 8)), 
+          axis.title.y = element_text(color = 'black', size = 12, margin = margin(r = 8)), 
           panel.grid = element_line(color = 'white'), 
-          legend.box.background = element_rect(),
-          legend.box.margin = margin(4, 4, 4, 4), 
-          legend.position = "right", 
+          legend.position = "none", 
           legend.key.height = unit(1, 'cm'), 
           legend.title.align = -0.5)
   return(plt)
 }
 
 
-
-
-
-
-error_distribution_plot <- function(data, metric, methods, title = ''){
+#' error_distribution_facet
+#' 
+#' Function which creates a facet grid and displays the distribution of a specified
+#' error metric by method, P, and G. 
+#' @param data {dataframe}; Data frame containing complete simulation results.
+#' @param metric {string}; Desired criteria for imputation performance evaluation
+#' @param methods {list}; List of all desired methods to be displayed
+#' @param title {string}; Desired title for the returned plot
+#'
+error_distribution_facet <- function(data, metric, methods, title = ''){
   
-  # Performing filtering
-  data_filtered = data %>% dplyr::select(Method, all_of(metric)) %>%
-    dplyr::filter(Method %in% methods) %>%
+  # Defining method colors
+  method_colors = c(HWI = "chocolate4", LI = "grey50", NNI = "dodgerblue3")
+  
+  # Cleaning the data-frame
+  data_filtered = data %>% dplyr::filter(Method %in% methods) %>%
+    dplyr::select(Method, P, G, all_of(metric)) %>%
     dplyr::rename('value' = all_of(metric))
   
   # Creating the plot
-  plt = ggplot(data_filtered, aes(x = value, y = after_stat(count) / sum(after_stat(count)))) +
-    geom_histogram(fill = '#204176', color='#e9ecef', alpha = 0.9) +
-    facet_grid(~ factor(Method, levels = methods)) +
+  plt = ggplot(data = data_filtered, aes(x = value, fill = Method, group = Method)) +
+    facet_grid(rows = vars(factor(G, levels = c(50, 25, 10, 5))), cols = vars(factor(P))) +
+    geom_histogram(alpha = 0.55, color = 'black', position = 'identity') + 
+    geom_density(color = 'black', alpha = 0.6, adjust = 2) +
     labs(title = title, 
-         x = metric, 
-         y = 'Frequency') +
+         subtitle = paste0('Metric: ', metric),
+         x = 'Missing Proportion (P)', 
+         y = 'Gap Width (G)', 
+         fill = 'Method:') +
     theme_bw() +
+    scale_fill_manual(values = method_colors) +
     theme(plot.title = element_text(hjust = 0.5, face = 'bold', size = 14), 
-          strip.background = element_rect(fill = 'white'), 
-          strip.text = element_text(color = 'black', face = 'bold', size = 12), 
-          panel.spacing = unit(0.8, 'lines'), 
-          axis.text = element_text(color = 'black', size = 12), 
-          axis.title.x = element_text(color = 'black', size = 14, margin = margin(t = 8)), 
-          axis.title.y = element_text(color = 'black', size = 14, margin = margin(r = 8)), 
+          plot.subtitle = element_text(hjust = 0.5, size = 12, face = 'italic'),
+          strip.background = element_rect(fill = 'grey75'), 
+          strip.text = element_text(color = 'black', face = 'bold', size = 10),
+          strip.text.y = element_text(angle = 0),
+          panel.spacing = unit(0.3, 'lines'), 
+          axis.text = element_text(color = 'black', size = 8), 
+          axis.title.x = element_text(color = 'black', size = 12, margin = margin(t = 8)), 
+          axis.title.y = element_text(color = 'black', size = 12, margin = margin(r = 8)), 
           legend.box.background = element_rect(),
           legend.box.margin = margin(4, 4, 4, 4), 
           legend.position = "right", 
           legend.key.height = unit(1, 'cm'), 
-          legend.title.align = -0.5)
+          legend.title.align = -0.5, 
+          legend.background = element_rect(fill = 'white'))
   return(plt)
 }
-
 
 
 
