df8d2a513ab030e242d494006d61f5d87d6ed428
Evan-Callaghan <callaghan.evan@gmail.com>
2023-12-04 13:49:20 -0500

    Prelim sim update

--- APPROACHES/Testing/preliminary.R
+++ APPROACHES/Testing/preliminary.R
@@ -3,45 +3,25 @@
 ##########################################
 
 
-#* Requirements:
-#*    tensorflow version ...
-#*    keras version ...
-#*    Three data sets (as csv files)
-
-
 
 ## Importing libraries
 ## -----------------------
 
-#library(parallel)
 library(tensorflow)
 library(keras)
 library(interpTools)
-#library(tictoc)
 # install_tensorflow(envname = "r-tensorflow")
-
-
 
 
 ## Configuring GPU set-up
 ## -----------------------
 
 ## NOTE: R Project set up to use virtual Python version
-gpus = tf$config$experimental$list_physical_devices('GPU')
+# gpus = tf$config$experimental$list_physical_devices('GPU')
 # for (gpu in gpus){
 #   tf$config$experimental$set_memory_growth(gpu, TRUE)
 # }
 
-
-# tf.debugging.set_log_device_placement(True)
-# gpus = tf.config.list_logical_devices('GPU')
-# strategy = tf.distribute.MirroredStrategy(gpus)
-# with strategy.scope():
-#   inputs = tf.keras.layers.Input(shape=(1,))
-# predictions = tf.keras.layers.Dense(1)(inputs)
-# model = tf.keras.models.Model(inputs=inputs, outputs=predictions)
-# model.compile(loss='mse',
-#               optimizer=tf.keras.optimizers.SGD(learning_rate=0.2))
 
 
 ## Importing functions
@@ -50,90 +30,102 @@ gpus = tf$config$experimental$list_physical_devices('GPU')
 source('Simulations/Preliminary/Code/autoencoder_helper.R')
 
 
+## Generating Time Series Data-Frames
+## -----------------------
+
+generate_df <- function(data, P, G, K){
+  
+  # Defining helpful parameters
+  N = length(data); M = length(P) * length(G) * K
+  
+  # Initializing
+  names = c(); columns = c()
+  
+  # Scaling the input data
+  data_scaled = (data - min(data)) / (max(data) - min(data))
+  
+  # Simulating gaps using interpTools
+  set.seed(222)
+  data0 = interpTools::simulateGaps(list(data_scaled), P, G, K)
+  
+  # Extracting data from interpTools nested lists
+  for (p in P){
+    for (g in G){
+      for (k in 1:K){
+        names = c(names, paste0('X', p, '_', g, '_', k))
+        function_call = paste0('columns = c(columns, data0[[1]]$p', p, '$g', g, '[[', k, ']])')
+        eval(parse(text = function_call))
+      }
+    }
+  }
+  # Formatting the data-frame with proper column names
+  data0 = as.data.frame(matrix(columns, nrow = N, byrow = FALSE))
+  colnames(data0) = names
+  
+  # Formatting the complete time series into a data-frame
+  data_df = data.frame('data' = data_scaled)
+  
+  # Sanity Check:
+  for (m in 1:M){
+    print(paste0('P:', estimate_p(data0[,m]), '  G:', estimate_g(data0[,m])))
+  }
+  
+  return(list(data_df, data0))
+}
+
+
 ## Simulation Parameters
 ## -----------------------
+
+MODELS = c(1)
+TRAIN_SIZE = c(500, 1000, 2000)
+BATCH_SIZE = c(25, 50, 100)
 
 P = c(0.1, 0.2, 0.3, 0.4)
 G = c(5, 10, 25, 50)
 K = 5
-
-TRAIN_SIZE = c(640, 1280, 2560)
-EPOCHS = c(25, 50, 100)
-BATCH_SIZE = c(16, 32, 64)
-
-
-
-P = c(0.1)
-G = c(5)
-K = 5
-
-TRAIN_SIZE = c(320, 640, 1280)
-EPOCHS = c(25, 50, 75)
-BATCH_SIZE = c(16, 32, 64)
-
-
-## Reading the data sets
-## -----------------------
-
-sunspots = read.csv('Data/Cleaned/sunspots.csv')$sunspots
-apple = read.csv('Data/Cleaned/apple.csv')$apple
-temperature = read.csv('Data/Cleaned/toronto_temperature.csv')$temperature
-high_snr = read.csv('Data/Cleaned/simulated.csv')$high_snr
-low_snr = read.csv('Data/Cleaned/simulated.csv')$low_snr
-modulated = read.csv('Data/Cleaned/simulated.csv')$modulated
 
 
 
 ## Simulation 1: Sunspots Data
 ## -----------------------
 
-# Creating custom models for sunspots data
-model1 = create_model(N = length(sunspots), units = 64, connected_units = 16, 'relu')
-model2 = create_model(N = length(sunspots), units = 128, connected_units = 32, 'relu')
-MODELS = c(model1, model2)
+# Reading sunspots time series data
+sunspots = read.csv('Data/Cleaned/sunspots.csv')$sunspots
+
+# Creating sunspots data-frames
+sunspots_df = generate_df(sunspots, P, G, K)
+data = sunspots_df[[1]]; data0 = sunspots_df[[2]]
 
 # Running the imputation simulation
-sunspots_sim = simulation_main(sunspots, P, G, K, MODELS, TRAIN_SIZE, EPOCHS, BATCH_SIZE)
+sunspots_sim = simulation(data, data0, MODELS, TRAIN_SIZE, BATCH_SIZE)
 
 # Exporting simulation performance as a csv file
-write.csv(sunspots_sim, 'Simulations/Preliminary/Results/Prelim_November2023_sunspots100.csv', row.names = FALSE)
+write.csv(sunspots_sim, 'Simulations/Preliminary/Results/Prelim_sunspots.csv', row.names = FALSE)
 
 # Observed simulation time: ____
-
-
-
-data1 = read.csv('Simulations/Preliminary/Results/Prelim_November2023_sunspots.csv')
-data2 = read.csv('Simulations/Preliminary/Results/Prelim_November2023_sunspots2.csv')
-data3 = read.csv('Simulations/Preliminary/Results/Prelim_November2023_sunspots3.csv')
-data4 = read.csv('Simulations/Preliminary/Results/Prelim_November2023_sunspots4.csv')
-data5 = read.csv('Simulations/Preliminary/Results/Prelim_November2023_sunspots5.csv')
-
-
-head(data1)
-head(data2)
-head(data3)
-head(data4)
-head(data5)
+View(sunspots_sim)
 
 
 
 ## Simulation 2: Apple Stock Data
 ## -----------------------
 
-# Creating custom models for apple data
-model1 = create_model(N = length(apple), units = 64, connected_units = 32, 'relu')
-model2 = create_model(N = length(apple), units = 128, connected_units = 32, 'relu')
-MODELS = c(model1, model2)
+# Reading apple time series data
+apple = read.csv('Data/Cleaned/apple.csv')$apple
+
+# Creating apple data-frames
+apple_df = generate_df(apple, P, G, K)
+data = apple_df[[1]]; data0 = apple_df[[2]]
 
 # Running the imputation simulation
-tic('Apple Simulation')
-apple_sim = simulation_main(apple, P, G, K, MODELS, TRAIN_SIZE, EPOCHS, BATCH_SIZE)
-toc()
+apple_sim = simulation(data, data0[,c(5,6)], MODELS, TRAIN_SIZE, BATCH_SIZE)
 
 # Exporting simulation performance as a csv file
-write.csv(apple_sim, 'Simulations/Preliminary/Results/Prelim_November2023_apple.csv', row.names = FALSE)
+write.csv(apple_sim, 'Simulations/Preliminary/Results/Prelim_apple.csv', row.names = FALSE)
 
 # Observed simulation time: ____
+View(apple_sim)
 
 
 
@@ -311,4 +303,33 @@ write.csv(modulated_data[[2]], 'Data/Exported/modulated_data0.csv', row.names = 
 
 
 
+
+
+
+library(dplyr)
+
+data = read.csv('Data/Prelim_Autoencoder_November2023_sunspots.csv')
+dim(data)
+
+data_grouped = data %>% dplyr::group_by(P, G, Model, Train.Size, Batch.Size) %>% 
+  dplyr::summarise(MAE = mean(MAE), RMSE = mean(RMSE), LCL = mean(LCL))
+
+View(data_grouped %>% dplyr::filter(P == 0.1, G == 5))
+View(data_grouped %>% dplyr::filter(P == 0.2, G == 25))
+
+
+library(gt)
+
+P = 0.25
+
+data_filtered = data_grouped %>% data.frame() %>% 
+  dplyr::filter(P == 0.2, G == 25) %>%
+  dplyr::select(c(Model, Train.Size, Batch.Size, MAE, RMSE, LCL)) %>%
+  dplyr::mutate(MAE = round(MAE, 4), RMSE = round(RMSE, 4), LCL = round(LCL, 4))
+
+
+data_filtered %>% gt() %>%
+  gt::tab_spanner(label = md('**Autoencoder Parameters**'), columns = c(Model, Train.Size, Batch.Size)) %>%
+  gt::tab_spanner(label = md('**Performance**'), columns = c(RMSE, MAE, LCL)) %>%
+  gt::tab_header(title = md("**Simulation Performance**"), subtitle = paste0())
 
